801b0dbf3e61d1654a3e1113396bcc190c5943ca
dvicjon <devinvjones@gmail.com>
2021-03-08 12:01:36 -0500

    working on categories.js

---
+++ public/assets/js/categories.js
@@ -0,0 +1,51 @@
+const $categoryForm = document.querySelector('#categories-form');
+const $displayArea = document.querySelector('#display-area');
+
+const printResults = resultArr => {
+    console.log(resultArr);
+
+    const categoryHTML = resultArr.map(({ id, name }) => {
+        return `
+        
+        <div>
+            <div data-id=${id}>
+                <h4>${name}</h4>
+            </div>
+        </div>
+        
+        `;
+    });
+
+    $displayArea.innerHTML = categoryHTML.join('');
+};
+
+const getCategories = (formData = {}) => {
+    let queryUrl = '/api/categories?';
+
+    Object.defineProperties(formData).forEach(([key, value]) => {
+        queryUrl += `${key}=${value}&`;
+    });
+
+    console.log(queryUrl);
+    fetch(queryUrl)
+    .then(response => {
+        if(!response.ok) {
+            return aler('Error: ' + response.statusText);
+        }
+        return response.json(); 
+    }).then(categoryData => {
+        console.log(categoryData);
+        printResults(categoryData);
+    });
+};
+
+const handleGetCategoriesSubmit = event => {
+    event.preventDefault(); 
+    const name = $categoryForm.querySelector('[name="category-name"');
+    const categoryObject = { name };
+    getCategories(categoryObject);
+};
+
+$categoryForm.addEventListener('submit', handleGetCategoriesSubmit);
+
+getCategories(); 
